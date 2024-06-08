a3459d9cf6afb2546134f610132624ec2871c3d9
Lucas <lchen@dustyrobotics.com>
2024-06-07 17:31:21 -0700

    re

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+from math import radians
 import time
 
 import cv2
@@ -6,6 +7,7 @@ import depthai as dai
 import numpy as np
 
 from yolo_api import Segment
+from scipy.ndimage import sobel
 
 
 def sigmoid(x):
@@ -33,17 +35,21 @@ def main():
     pipeline = dai.Pipeline()
 
     # Init pipeline's output queue
-    xout_rgb = pipeline.createXLinkOut()
-    xout_yolo_nn = pipeline.createXLinkOut()
-    xout_depth = pipeline.createXLinkOut()
-    xout_rgb.setStreamName("rgb")
-    xout_yolo_nn.setStreamName("yolo_nn")
-    xout_depth.setStreamName("depth")
+    # xout_rgb = pipeline.createXLinkOut()
+    # xout_yolo_nn = pipeline.createXLinkOut()
+    # xout_depth = pipeline.createXLinkOut()
+    sync = pipeline.create(dai.node.Sync)
+    xoutGrp = pipeline.create(dai.node.XLinkOut)
+    xoutGrp.setStreamName("xout")
+    # xout_rgb.setStreamName("rgb")
+    # xout_yolo_nn.setStreamName("yolo_nn")
+    # xout_depth.setStreamName("depth")
+
 
     # Neural network pipeline properties
     nn = pipeline.createNeuralNetwork()
     nn.setBlobPath(path_to_yolo_blob)
-    nn.out.link(xout_yolo_nn.input)
+    nn.out.link(sync.inputs["xout_yolo_nn"])
 
     # Color cam properties (Cam_A/RGB)
     cam_rgb = pipeline.createColorCamera()
@@ -51,7 +57,7 @@ def main():
     cam_rgb.setInterleaved(False)
     cam_rgb.setResolution(dai.ColorCameraProperties.SensorResolution.THE_800_P)
     cam_rgb.preview.link(nn.input)
-    cam_rgb.preview.link(xout_rgb.input)
+    cam_rgb.preview.link(sync.inputs["xout_rgb"])
     print("Color cam resolution: ", cam_rgb.getResolutionSize())
 
     # Stereo/Depth node properties
@@ -62,7 +68,10 @@ def main():
     stereo.setLeftRightCheck(True)
     stereo.setSubpixel(True)
     stereo.setExtendedDisparity(True)
-    stereo.depth.link(xout_depth.input)
+    stereo.depth.link(sync.inputs["xout_depth"])
+
+    # Link
+    sync.out.link(xoutGrp.input)
 
     # Left cam properties (Cam_B/Mono)
     left = pipeline.create(dai.node.MonoCamera)
@@ -83,38 +92,41 @@ def main():
 
     device.setIrLaserDotProjectorBrightness(200) # 0 to 1200
 
-    rgb_queue = device.getOutputQueue("rgb", maxSize=2, blocking=False)
-    yolo_nn_queue = device.getOutputQueue("yolo_nn", maxSize=2, blocking=False)
-    depth_queue = device.getOutputQueue("depth", maxSize=2, blocking=False)
-
-    idx = 0
-
-    rgb_history = []
+    # rgb_queue = device.getOutputQueue("rgb", maxSize=2, blocking=False)
+    # yolo_nn_queue = device.getOutputQueue("yolo_nn", maxSize=2, blocking=False)
+    # depth_queue = device.getOutputQueue("depth", maxSize=2, blocking=False)
+    sync_queue = device.getOutputQueue("xout", 5, False)
 
     while True:
         if cv2.waitKey(1) == ord("q"):
             break
 
-        rgb_queue_msg = rgb_queue.get()
-        yolo_nn_queue_msg = yolo_nn_queue.get()
-        depth_queue_msg = depth_queue.get()
+        # rgb_queue_msg = rgb_queue.get()
+        # yolo_nn_queue_msg = yolo_nn_queue.get()
+        # depth_queue_msg = depth_queue.get()
 
-        if rgb_queue_msg is None:
-            print("rgb_queue message is empty")
-            continue
+        # if rgb_queue_msg is None:
+        #     print("rgb_queue message is empty")
+        #     continue
         
-        # Artificially use an older rgb frame so they are ~kinda~ in sync
-        new_frame = rgb_queue_msg.getCvFrame()
-        rgb_history.append(new_frame)
-        if len(rgb_history) > 2:
-            rgb_history.pop(0)
-        frame = rgb_history[0]
+        # depth_frame = (
+        #     depth_queue_msg.getFrame() if depth_queue_msg is not None else None
+        # )  # depth frame returns values in mm (millimeter)
 
-        depth_frame = (
-            depth_queue_msg.getFrame() if depth_queue_msg is not None else None
-        )  # depth frame returns values in mm (millimeter)
+        # if yolo_nn_queue_msg is None or depth_frame is None:
+        #     continue
 
-        if yolo_nn_queue_msg is None or depth_frame is None:
+        message_group = sync_queue.get()
+        for name, msg in message_group:
+            match name:
+                case "xout_rgb":
+                    frame = msg.getCvFrame()
+                case "xout_depth":
+                    depth_frame = msg.getCvFrame()
+                case "xout_yolo_nn":
+                    yolo_nn_queue_msg = msg
+
+        if frame is None or depth_frame is None or yolo_nn_queue_msg is None:
             continue
 
         output0 = np.reshape(
@@ -126,6 +138,17 @@ def main():
             yolo_nn_queue_msg.getLayerFp16("output1"),
             newshape=(output1_shape),
         )
+
+        # rows = [
+        #     np.concatenate((output1[0, 0:8]), axis=1),
+        #     np.concatenate((output1[0, 8:16]), axis=1),
+        #     np.concatenate((output1[0, 16:24]), axis=1),
+        #     np.concatenate((output1[0, 24:32]), axis=1),
+        # ]
+
+        # bigone = np.concatenate(rows, axis=0)
+
+        # cv2.imshow("All Prototypes", bigone)
 
         if len(output0) == 0 or len(output1) == 0:
             continue
@@ -150,8 +173,7 @@ def main():
         print("Prototype shape ", output1.shape)
         # print("Coefficient shape ", yoloseg.mask_pred.shape)
 
-        # mask = sigmoid(output1[0, 2]) # Shape [C H W]
-
+        # Optimized protos
         proto_sum = np.zeros(output1.shape[2:4], float)
         # Find the mask coefficients with highest adherence to dot
         for proto in output1[0]:
@@ -163,12 +185,38 @@ def main():
             scale = var
 
             proto_sum += (proto - mean) * scale
+        
+        # Manual protos (Good for objects)
+        # proto_sum = output1[0, 1] + output1[0, 2] + output1[0, 4] + output1[0, 6] + -1 * output1[0, 16] + -1 * output1[0, 21]
 
-        mask = sigmoid(proto_sum * 3)
+        # mask = sigmoid(proto_sum * 3)
 
-        print("Mask", mask.shape, round(mask.min(), 3), round(mask.max(), 3))
+        # Edge
+        sobelY = sobel(proto_sum, axis=0)
+        sobelX = sobel(proto_sum, axis=1)
+        sobelS = np.sqrt(sobelX ** 2 + sobelY ** 2)
+        sobelA = np.arctan2(sobelY, sobelX)
+        sobelA = np.minimum(np.abs(sobelA), np.abs(sobelA - np.pi)) # Angle distance from X axis
 
-        rgb = [0.0, 1.0, 0.0]
+        print("Mask Sobel", sobelS.shape, round(sobelS.min(), 3), round(sobelS.max(), 3))
+
+        # Get bottoms of mask
+        sobelS = np.clip(sobelS / 11, 0, 1) # N
+        mask = np.where((sobelY > 0) & (sobelS > 0.4) & (sobelA > radians(10)), sobelS, 0)
+
+        # Make sure edges do not exceed lowest contour
+        y_coords = np.arange(mask.shape[0])[:, np.newaxis]  # Hx1
+        grid = np.tile(y_coords, (1, mask.shape[1]))
+        print("Meshgrid", grid.shape)
+        grid[mask == 0] = 0
+
+        lowest_indices = np.argmax(grid, axis=0)
+        lowest_acceptable = lowest_indices - output1.shape[2] // 8
+        # Set values of mask with y > lowest_acceptable to 0
+        row_indices = np.arange(mask.shape[0])[:, np.newaxis] # Hx1
+        mask[row_indices < lowest_acceptable] = 0
+
+        rgb = [0.3, 0.0, 1.0]
         maskDisp = np.dstack((rgb[0] * mask, rgb[1] * mask, rgb[2] * mask))
         maskDisp[..., 2][dot] = 0.99
         maskDispUpscaled = cv2.resize(maskDisp, (frame.shape[1], frame.shape[0]), interpolation=cv2.INTER_LINEAR)
@@ -180,12 +228,11 @@ def main():
             (maskDispUpscaled * 255).astype(np.uint8), 
             mask_alpha, frame, 1 - mask_alpha, 0)
         
-        idx += 1
-
         cv2.imshow("Output", frame)
 
         print('Depth max', depth_frame.max(), depth_frame.dtype)
-        depthFrameScaled = (np.clip(depth_frame / 2000, 0, 1) * 255).astype(np.uint8)
+
+        depthFrameScaled = (np.clip(depth_frame / 10000, 0, 1) * 255).astype(np.uint8)
         depthFrameDisp = cv2.applyColorMap(depthFrameScaled, cv2.COLORMAP_JET)
         cv2.imshow('Depth', depthFrameDisp)
 
